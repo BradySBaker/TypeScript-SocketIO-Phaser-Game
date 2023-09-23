b3357d11ca02f7f5317944959ba2f87d916b5ded
BradySBaker <bradysamuelbaker@gmail.com>
2023-09-22 21:09:05 -0700

    Player movement and creation working on client and server

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,14 @@
 import { Socket, Server } from 'socket.io';
 
+let playerCount: number = 0;
+
+type Player = {
+  x: number,
+  y: number
+};
+
+let playerPositions: Player[] = [];
+
 const io = new Server(3000, {
   cors: {
     origin: ['http://localhost:2000']
@@ -7,9 +16,19 @@ const io = new Server(3000, {
 });
 
 io.on('connection', (socket: Socket) => {
-  console.log('connection ' + socket.id);
+  const newPlayer: Player = { x: 0, y: 0 };
+  playerPositions.push(newPlayer);
+  let playerId = playerCount;
+  io.to(socket.id).emit('playerData', playerPositions, playerId);
+  io.emit('newPlayer', playerPositions, playerId);
+  playerCount++;
+  socket.on('updatePosition', (pos: Player) => {
+    playerPositions[playerId] = pos;
+    io.emit('updatePosition', pos, playerId);
+  })
 });
 
-io.on('message', (message: string) => {
-  console.log(message);
+
+io.on('disconnect', () => {
+
 });
