128e3104076ac703f9198286021870392121a92e
BradySBaker <bradysamuelbaker@gmail.com>
2023-12-10 21:03:39 -0800

    Grapple checker done, grapple line created

--- client/src/scenes/controllers/GrappleHandler.ts
+++ client/src/scenes/controllers/GrappleHandler.ts
@@ -5,10 +5,15 @@ export default class GrappleHandler {
   game: Game;
   grappleHook!: Phaser.GameObjects.Sprite;
   graphics: Phaser.GameObjects.Graphics;
+  grappleCheckCircle!: Phaser.GameObjects.Ellipse;
+  collision = false;
+  grappling = false;
 
   constructor(game: Game) {
     this.game = game;
-    this.graphics = this.game.add.graphics();
+    this.graphics = game.add.graphics();
+    this.grappleCheckCircle = game.add.ellipse(0, 0, 50, 50, 0xFFFFFF, 1).setDepth(1).setAlpha(0);
+    game.physics.add.existing(this.grappleCheckCircle);
   }
 
   handlePosition(player: Player) {
@@ -20,18 +25,31 @@ export default class GrappleHandler {
   }
 
   handleGrapple() {
+    if (this.grappling) {
+      return;
+    }
     this.graphics.clear();
     if (this.grappleHook.name === 'badAngle') {
       return;
     }
+    let mouseWorldX = this.game.cameras.main.getWorldPoint(this.game.input.x, this.game.input.y).x;
+    let mouseWorldY = this.game.cameras.main.getWorldPoint(this.game.input.x, this.game.input.y).y;
+
+    this.grappleCheckCircle.x = mouseWorldX;
+    this.grappleCheckCircle.y = mouseWorldY;
+
+    if (this.game.physics.overlap(this.grappleCheckCircle, this.game.PlatformHandler.platformGroup)) {
+      this.grappleCheckCircle.alpha = 1;
+    } else {
+      this.grappleCheckCircle.alpha = 0;
+    }
+
     if (this.game.input.activePointer.isDown && this.grappleHook) {
-      let mouseWorldX = this.game.cameras.main.getWorldPoint(this.game.input.x, this.game.input.y).x;
-      let mouseWorldY = this.game.cameras.main.getWorldPoint(this.game.input.x, this.game.input.y).y;
       this.graphics.beginPath();
       this.graphics.moveTo(this.grappleHook.x, this.grappleHook.y);
       this.graphics.lineTo(mouseWorldX, mouseWorldY);
       this.graphics.strokePath();
-
+      this.grappling = true;
     }
   }
 }
--- client/src/scenes/objects/PlatformHandler.ts
+++ client/src/scenes/objects/PlatformHandler.ts
@@ -2,10 +2,13 @@ import Game from "../game";
 
 export default class PlatformHandler {
   game: Game;
+  platformGroup!: Phaser.GameObjects.Group;
   constructor(game: Game) {
     this.game = game;
+    this.platformGroup = game.physics.add.group({classType: Phaser.GameObjects.Rectangle});
   }
   spawnPlatforms() {
-    this.game.add.rectangle(100, 500, 3000, 30, 0xfffff);
+    let platform = this.game.add.rectangle(100, 500, 3000, 30, 0xfffff);
+    this.platformGroup.add(platform);
   }
 }
