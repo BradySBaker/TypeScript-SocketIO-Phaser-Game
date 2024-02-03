debd7b4b2cccb37da65963966805cedbc4ede78d
BradySBaker <bradysamuelbaker@gmail.com>
2024-02-02 17:59:32 -0800

    Fixed socket issue

--- craftingRecipes.ts
+++
@@ -1,9 +0,0 @@
-// 1: 'bone',
-// 2: 'goo',
-// 3: 'stone',
-// 4: 'spear'
-type Drop = 'bone' | 'stone' | 'goo' | 'spear' | 'bone_pickaxe';
-let recipes: {[result in Drop]?: {[item in Drop]?: number}} = {
-  'bone_pickaxe': {'bone': 2, 'goo': 2},
-}
-export default recipes;
